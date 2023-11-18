b45ec55cbf2ab4fa899f909c477fe57d3b66279b
Tanzim <tanzim008@gmail.com>
2023-11-18 01:20:41 -0330

    added states

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 // Game State
 let phase = 'waiting'; //waiting | stretching | turning | walking | transitioning | falling
 let timestamp; //The timestamp of the previous animation cycle
+let heroX;
+let heroYl
+let sceneOffset;
+
+let platform = [];
+let sticks = [];
+
+let score = 0;
+
+// Config
+// Getting the canvas element
+const canvas = document.getElementById('#game');
+// Getting the drawing context
+const ctx = canvas.getContext('2d'); 
+
+// Further UI elements
+const scoreElement = document.getElementById("score");
+const restartButton = document.getElementById("restart");
+
+// Start game
+resetGame();
+
+// Resets game state and layout
+function resetGame() {
+    
+  
+    draw();
+}
+
+function draw() {
+
+}
+window.addEventListener('mousedown', (event)=>{
+
+});
+window.addEventListener('mouseup', (event)=>{
+
+});
+function animate(timestamp){
+
+}
